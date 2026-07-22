905f65b0f541a6abbe7223742103b19e496145c8
NhanLocal <nhannhd1004@gmail.com>
2026-07-22 13:57:34 +0700

    Initial commit

---
+++ src/test/java/com/nhan/sp2/Sp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhan.sp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
